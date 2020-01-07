0166eca5269d99405c5a104a65c8f69e8d48f6a0
Ubuntu <ubuntu@ip-172-31-9-78.us-east-2.compute.internal>
2020-01-07 08:44:24 +0000

    agg validation.

--- apps/kg/train_pytorch.py
+++ apps/kg/train_pytorch.py
@@ -29,7 +29,7 @@ def load_model_from_checkpoint(logger, args, n_entities, n_relations, ckpt_path)
     model.load_emb(ckpt_path, args.dataset)
     return model
 
-def train(args, model, train_sampler, valid_samplers=None):
+def train(args, model, train_sampler, valid_samplers=None, queue=None):
     if args.num_proc > 1:
         th.set_num_threads(1)
     logs = []
@@ -41,6 +41,8 @@ def train(args, model, train_sampler, valid_samplers=None):
     update_time = 0
     forward_time = 0
     backward_time = 0
+
+    valid_metrics = []
     for step in range(args.init_step, args.max_step):
         start1 = time.time()
         pos_g, neg_g = next(train_sampler)
@@ -60,7 +62,7 @@ def train(args, model, train_sampler, valid_samplers=None):
         update_time += time.time() - start1
         logs.append(log)
 
-        if step % args.log_interval == 0:
+        if (step + 1) % args.log_interval == 0:
             for k in logs[0].keys():
                 v = sum(l[k] for l in logs) / len(logs)
                 print('[Train]({}/{}) average {}: {}'.format(step, args.max_step, k, v))
@@ -75,10 +77,13 @@ def train(args, model, train_sampler, valid_samplers=None):
             backward_time = 0
             start = time.time()
 
-        if args.valid and step % args.eval_interval == 0 and step > 1 and valid_samplers is not None:
-            start = time.time()
-            test(args, model, valid_samplers, mode='Valid')
-            print('test:', time.time() - start)
+        if args.valid and (step + 1) % args.eval_interval == 0 and valid_samplers is not None:
+            #start = time.time()
+            metrics = test(args, model, valid_samplers, mode='Valid')
+            valid_metrics.append(metrics)
+            #print('test:', time.time() - start)
+    if queue is not None:
+        queue.put(valid_metrics)
 
 def test(args, model, test_samplers, mode='Test', queue=None):
     if args.num_proc > 1:
@@ -97,8 +102,9 @@ def test(args, model, test_samplers, mode='Test', queue=None):
                 metrics[metric] = sum([log[metric] for log in logs]) / len(logs)
         if queue is not None:
             queue.put(metrics)
-        else:
-            for k, v in metrics.items():
-                print('{} average {} at [{}/{}]: {}'.format(mode, k, args.step, args.max_step, v))
+        #else:
+        #    for k, v in metrics.items():
+        #        print('{} average {} at [{}/{}]: {}'.format(mode, k, args.step, args.max_step, v))
     test_samplers[0] = test_samplers[0].reset()
     test_samplers[1] = test_samplers[1].reset()
+    return metrics
